bcc2aef2bb416af3bbe66dd705180c4fa0078099
amirmir <a_mirzayi@yahoo.com>
2011-09-21 15:05:00 +0200

    some configuration changes.

--- hibernatespring/src/main/java/my/samples/hibernatespring/App.java
+++ hibernatespring/src/main/java/my/samples/hibernatespring/App.java
@@ -1,5 +1,11 @@
 package my.samples.hibernatespring;
 
+import my.samples.hibernatespring.dao.StockDaoImpl;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+
 /**
  * Hello world!
  *
@@ -8,6 +14,8 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        ApplicationContext context = new ClassPathXmlApplicationContext(new String[] { "META-INF/springsample.xml" });
+        StockDaoImpl stockDao = (StockDaoImpl) context.getBean("stockDaoImpl");
+        stockDao.findStockById(1);
     }
 }
